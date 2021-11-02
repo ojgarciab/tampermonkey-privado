6a60dbf58abc90ccb37c52c8379d73c94d949da6
Oscar García <ojgarciab@users.noreply.github.com>
2021-11-02 15:46:38 +0100

    Update indiegala.user.js

--- indiegala.user.js
+++ indiegala.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filtro de Indiegala
 // @namespace    http://linaresdigital.com/
-// @version      0.6
+// @version      0.7
 // @description  Filtro de regalos para indiegala
 // @author       Oscar Garcia
 // @match        https://www.indiegala.com/giveaways
@@ -14,19 +14,17 @@
 (function() {
     'use strict';
 
-    console.log("Cargado... v0.6");
     /* ... */
-    const header = document.querySelector("header.default-header");
-    const altura = header.offsetTop + header.offsetHeight;
+    const contenedor = document.querySelector("section.main-container")
 
+    console.log("Cargado... v0.7");
     let style = document.createElement("style");
 	style.appendChild(document.createTextNode(""));
 	document.head.appendChild(style);
-    //style.sheet.insertRule("#propio { position: fixed;  z-index: 10000; top: calc(" + altura + "px + 1em); right: 1em; opacity: 0.8; }", 0);
-    style.sheet.insertRule("#propio { float: right;  margin-right: 1em; opacity: 0.8; }", 0);
+    style.sheet.insertRule("#propio { position: absolute; margin-top: 1em; right: 1em; opacity: 0.8; border-radius: 30px; background: red; padding: 1em; }", 0);
 
     let div = document.createElement("div");
     div.id = "propio";
-    div.innerHTML = '<input type="number" value="100" min="0" max="240" name="valor" /><input type="number" value="100" min="0" max="100" name="nivel" /><button type="button">Filtrar</button>';
-    document.querySelector("section.main-container").appendChild(div);
+    div.innerHTML = 'V: <input type="number" value="100" min="0" max="240" name="valor" />, N: <input type="number" value="0" min="0" max="100" name="nivel" /><button type="button">Filtrar</button>';
+    contenedor.insertBefore(div, contenedor.firstChild);
 })();

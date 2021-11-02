13c8a3513c4c844c732dd71ccb4b7e9e75f30286
Oscar García <ojgarciab@users.noreply.github.com>
2021-11-02 13:07:15 +0100

    Create indiegala.js
    Filtro de indiegala

---
+++ indiegala.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Filtro de Indiegala
+// @namespace    http://linaresdigital.com/
+// @version      0.1
+// @description  Filtro de regalos para indiegala
+// @author       Oscar Garcia
+// @match        https://www.indiegala.com/giveaways
+// @icon         https://www.google.com/s2/favicons?domain=indiegala.com
+// @grant        none
+// @downloadURL
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    /* ... */
+    console.log("Cargado...");
+})();

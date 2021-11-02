4ecf345063d24ac4f0523c1675398e55795bf11d
Oscar García <ojgarciab@users.noreply.github.com>
2021-11-02 13:16:48 +0100

    Update and rename indiegala.js to indiegala.user.js

--- indiegala.user.js
+++ indiegala.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Filtro de Indiegala
 // @namespace    http://linaresdigital.com/
-// @version      0.3
+// @version      0.4
 // @description  Filtro de regalos para indiegala
 // @author       Oscar Garcia
 // @match        https://www.indiegala.com/giveaways
 // @icon         https://www.google.com/s2/favicons?domain=indiegala.com
 // @grant        none
 // @run-at       document-end
-// @downloadURL  https://ojgarciab.github.io/tampermonkey-privado/indiegala.js
+// @downloadURL  https://ojgarciab.github.io/tampermonkey-privado/indiegala.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     /* ... */
-    console.log("Cargado... v2");
+    console.log("Cargado... v0.4");
 })();

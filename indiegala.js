f184bf29149b21fffcc8623cb00ad433c7e3135f
Oscar García <ojgarciab@users.noreply.github.com>
2021-11-02 13:08:24 +0100

    Update indiegala.js

--- indiegala.js
+++ indiegala.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Filtro de Indiegala
 // @namespace    http://linaresdigital.com/
-// @version      0.1
+// @version      0.2
 // @description  Filtro de regalos para indiegala
 // @author       Oscar Garcia
 // @match        https://www.indiegala.com/giveaways
 // @icon         https://www.google.com/s2/favicons?domain=indiegala.com
 // @grant        none
-// @downloadURL
+// @downloadURL  https://ojgarciab.github.io/tampermonkey-privado/indiegala.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     /* ... */
-    console.log("Cargado...");
+    console.log("Cargado... v2");
 })();

ba1353f8f78a699416f1dde4c50578d797fc22c2
Oscar García <ojgarciab@users.noreply.github.com>
2021-12-28 23:34:44 +0100

    Create twitch.user.js

---
+++ twitch.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Puntos de Twitch
+// @namespace    http://linaresdigital.com/
+// @version      0.7
+// @description  Obtención de puntos de Twitch
+// @author       Oscar Garcia
+// @match        https://www.indiegala.com/giveaways
+// @exclude      https://www.twitch.tv/friends*
+// @exclude      https://www.twitch.tv/wallet*
+// @exclude      https://www.twitch.tv/subscriptions*
+// @exclude      https://www.twitch.tv/drops*
+// @exclude      https://www.twitch.tv/settings*
+// @icon         https://www.google.com/s2/favicons?domain=twitch.tv
+// @grant        none
+// @run-at       document-end
+// @downloadURL  https://ojgarciab.github.io/tampermonkey-publico/twitch.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(() => {
+        let boton = document.querySelector('button[aria-label="Reclamar bonificación"]');
+        if (boton !== null) {
+            console.log("Pulsado");
+            boton.click();
+        }
+    }, 1000);
+})();
